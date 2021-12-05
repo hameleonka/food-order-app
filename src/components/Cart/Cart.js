0450a064bc8c93277235062a92f670234ea00076
Natalia Milovanova <hameleonka28@gmail.com>
2021-12-04 22:47:57 -0500

    Adds state to Cart and Meals components

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ function Cart(props) {
   const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(item => <li>{item.name}</li>)
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       <ul className={classes['cart-items']}> {cartItems} </ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>37</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
